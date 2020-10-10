a1b26ca59f6d629e879429a57ea08853fab002b1
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-10-10 14:03:50 +0200

    Add files via upload
    123

---
+++ lec_1_math_oper.py
@@ -0,0 +1,15 @@
+print(3+4) #slogenie
+
+print(4-3) #вычитание
+
+print(3*4) #umnogenie
+
+print(7/3) #delenie
+
+print(7**2) #vozvedenie v stepen
+
+print(7**(-2)) 
+
+print(7//2) #tselaya chast ot deleniya
+
+print(7%2) #ostatok chast ot delenia
---
+++ lec_1_tip.py
@@ -0,0 +1,19 @@
+type #команда определения данных
+
+type(3)
+
+print(type(3))
+
+type(3.4)
+
+print(type(3.4))
+
+type('молодец')
+
+print(type('молодец'))
+
+a = 'молодец'
+
+print(a)
+
+print(type([1,2,3])
