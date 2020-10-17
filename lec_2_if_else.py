bd3a71f3ad978c625881216908d4333f738d6ccd
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-10-17 14:31:28 +0200

    я молодец

---
+++ lec_2_if_else.py
@@ -0,0 +1,6 @@
+a = int(input('введите a :'))
+
+if a>4:
+    print('hello 4')
+else:
+    print('hello a')
---
+++ lec_2_log_operation.py
@@ -0,0 +1,14 @@
+a = int(input('введите a :'))
+b = int(input('введите b :'))
+c = int(input('введите c :'))
+
+if a>4 and b==2:
+    print('Good')
+elif b>3 or c==5:
+    print('Best')
+else:
+    print('Bad')
+    
+a = 3
+b = 5
+c = -1
