832bf28e43acec1f0176febf27154f850686884e
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-11-21 14:57:43 +0200

    Update lub_3_task_3.py

--- lub_3_task_3.py
+++ lub_3_task_3.py
@@ -4,13 +4,14 @@ from lub_3_task_1 import acceleration_of_gravity as g
 import numpy as np
 x_0 = 0
 y_0 = 0
-t = np.linspace(0, 5, 100)
+n = 10
+t = np.linspace(0, 5, n)
 v_0 = 15
 a = 30 * (pi/180)
 x = x_0 + (v_0 * cos(a)*t)
 y = y_0 + (v_0 * sin(a)* t) - ((g * t**2)/2)
-A = np.zeros((100, 3))
-for i in range(100):
+A = np.zeros((n, 3))
+for i in range(n):
   A[i, 0] = t[i] 
   A[i, 1] = x[i]
   A[i, 2] = y[i]
