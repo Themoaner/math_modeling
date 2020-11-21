e3d73df100e03af39309a5387a6a682d3b602ccc
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-11-21 15:24:18 +0200

    Add files via upload

---
+++ lub_3_dop_1.py
@@ -0,0 +1,15 @@
+import numpy as np 
+M=int(input(' '))
+N=int(input(' '))
+A = np.zeros((N, M))
+B = np.zeros((N, M))
+C = np.zeros((N, M))
+print(A)
+print(B)
+for i in range:
+  for j in range:
+    if A[i][j] > B[i][j]:
+     C[i][j] = A[i][j]
+    else:
+      C[i][j] = B[i][j]
+print(C)
