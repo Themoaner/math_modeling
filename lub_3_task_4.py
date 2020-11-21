72975e38568d10c7385fd7987873f7d40d8a8efb
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-11-21 12:18:43 +0200

    Add files via upload

---
+++ lub_3_task_4.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numpy import sin
+N = int(input( ))
+M = int(input( ))
+A = np.zeros((N,M))
+for i in range(N):
+  for j in range(M):
+    A[i][j] = sin(N*(i+1) + M*(j+1))
+    if A[i][j] < 0:
+      A[i][j] = 0 
+  
+print(A)
