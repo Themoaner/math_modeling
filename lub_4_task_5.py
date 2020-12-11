30bff8b8a4e68409a1a47c20cef70a7cc1a96fdc
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-12-12 00:01:22 +0200

    Update lub_4_task_5.py

--- lub_4_task_5.py
+++ lub_4_task_5.py
@@ -1,19 +1,19 @@
 from numpy import pi
 
-print('Введите фигуру (круг(c), прямоугольник(r), треугольник(t)) : ')
-p = str(input( ))
-
-if p=='c':
-  def my_func(R):
+def my_func(p,a=2,b=3,h=4,R=5):
+  """  
+  1-круг (введите R)
+  2-прямоугольник (введите a,b)
+  3-треугольник (введите a,h)
+  """
+  if p==1:
     x=pi*R**2
     print(x)
-elif p=='r':
-  def my_func(a, b):
+  elif p==2:
     x=a*b
     print(x)
-elif p=='t':
-  def my_func(a, h):
+  elif p==3:
     x=(a*h)/2
     print(x)
 
-my_func(int(input( )))
+my_func()
