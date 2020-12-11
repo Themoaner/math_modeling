9cf4f270c9dad3a1fc8d582531a607c4ecd05827
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-12-12 00:01:54 +0200

    Update lub_4_task_2.py

--- lub_4_task_2.py
+++ lub_4_task_2.py
@@ -1,12 +1,8 @@
-import numpy as np
-N = int(input())
-A = np.zeros((N))
-for i in range(N):
-  A[i] = int(input())
-print(A)
+def my_func(A):
+  b=1
+  for i in range(len(A)):
+    c=b*A[i]
+    b=c
+    print(c)
 
-def my_func(a):
-  x=np.prod(np.array(A))
-  print(x)
-
-my_func(A)
+my_func([1,2,3,4,5])
