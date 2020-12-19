aea22243c772c98eeead7f18fe9100742c92833c
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-12-19 13:40:12 +0200

    Update lub_6_4.py

--- lub_6_4.py
+++ lub_6_4.py
@@ -5,9 +5,9 @@ from np import exp
 from np import cos
 from np import sin
 
-def plotter(a=1,b=1,r=1,f=30, title='plotter'):
+def plotter(a=1,b=0.1,r=1,f=30, title='plotter'):
   f = np.arange(0, 8*pi, 0.1)
-  r = exp**(b*f)
+  r = exp(b*f)
 
   x=r*cos(f)
   y=r*sin(f)
@@ -17,6 +17,3 @@ def plotter(a=1,b=1,r=1,f=30, title='plotter'):
   plt.show()
 
 plotter()
-  
-  
-
