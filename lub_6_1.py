7eda91ea2c169fb085db6d695ded646b5514de68
Themoaner <72627928+Themoaner@users.noreply.github.com>
2020-12-19 13:16:48 +0200

    Update lub_6_1.py

--- lub_6_1.py
+++ lub_6_1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-plt.plot([1,1,5,5,1],[1,5,5,1,1],label='квадрат')
+plt.plot([1,1,5,5,1],[1,5,5,1,1],label='квадрат', marker='o', ms=5)
 plt.legend()
 plt.axis('equal')
 plt.show()
